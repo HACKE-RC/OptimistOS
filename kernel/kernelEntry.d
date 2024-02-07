fdfd8971bd69ef119028bb36b3dd814ba7b3e115
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2024-02-07 17:32:21 +0530

    major bug fix

--- kernel/kernelEntry.d
+++
@@ -1,7 +0,0 @@
-kernelEntry.o: kernelEntry.cpp kernelEntry.hpp renderer/BasicRenderer.hpp \
- renderer/../boot.h renderer/../../limine/limine.h \
- renderer/../printutils/e9print.h gdt/gdt.hpp idt/idt.hpp idt/isr.hpp \
- paging/pageFrameAllocator.hpp paging/../kernelEntry.hpp \
- paging/freeList.hpp paging/../lib/string.hpp paging/hashtable.hpp \
- paging/paging.hpp acpi/acpi.hpp acpi/../lib/string.hpp \
- acpi/../paging/paging.hpp
