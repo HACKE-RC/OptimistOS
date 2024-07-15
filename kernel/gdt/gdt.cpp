3b92986576c1b9bdbc157c4a8c0be86e534d279c
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2024-07-15 22:55:24 +0530

    Update gdt.cpp

--- kernel/gdt/gdt.cpp
+++ kernel/gdt/gdt.cpp
@@ -4,15 +4,15 @@
 extern "C" void gdtFlush(void* addr_t);
 
 gdtEntry gdt[] = {
-        {0, 0, 0, 0, 0, 0}, // null
-        {0xffff, 0, 0, 0x9a, 0x80, 0}, // 16-bit code
-        {0xffff, 0, 0, 0x92, 0x80, 0}, // 16-bit data
-        {0xffff, 0, 0, 0x9a, 0xcf, 0}, // 32-bit code
-        {0xffff, 0, 0, 0x92, 0xcf, 0}, // 32-bit data
-        {0, 0, 0, 0x9a, 0xa2, 0}, // 64-bit code
-        {0, 0, 0, 0x92, 0xa0, 0}, // 64-bit data
-        {0, 0, 0, 0xF2, 0, 0}, // user data
-        {0, 0, 0, 0xFA, 0x20, 0}, // user code
+        {0, 0, 0, 0, 0, 0},
+        {0xffff, 0, 0, 0x9a, 0x80, 0},
+        {0xffff, 0, 0, 0x92, 0x80, 0},
+        {0xffff, 0, 0, 0x9a, 0xcf, 0},
+        {0xffff, 0, 0, 0x92, 0xcf, 0},
+        {0, 0, 0, 0x9a, 0xa2, 0},
+        {0, 0, 0, 0x92, 0xa0, 0}, 
+        {0, 0, 0, 0xF2, 0, 0},
+        {0, 0, 0, 0xFA, 0x20, 0},
 };
 
 struct gdtPtrStruct gdtPtr;
